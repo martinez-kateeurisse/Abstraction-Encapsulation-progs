6264bfe3219aaafd3668136e6be78793111f28b9
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-01 21:57:00 +0800

    Program 1 - test file: Displaying Fan 1 status/values

--- test_fan.py
+++ test_fan.py
@@ -5,5 +5,11 @@ from fan_class import Fan
 
 #Create two fan objects
 fan1 = Fan(Fan.FAST, 10, "yellow", True)
+print("FAN 1 - STATUS")
+print("Fan Speed:", fan1.get_speed())
+print("Fan Radius:", fan1.get_radius())
+print("Fan Color:", fan1.get_color())
+print("Fan Power (On):", fan1.get_power())
+
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 

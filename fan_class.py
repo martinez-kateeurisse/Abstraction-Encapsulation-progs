382770b1a3d091a61c0edf540cd047074c96e83f
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-01 20:26:25 +0800

    Program 1-fan_class: Fan class - created

--- fan_class.py
+++ fan_class.py
@@ -11,6 +11,7 @@
 #Write a test program named TestFan that creates two Fan objects. For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
 
 #Create a class
+class Fan():
     #Create three constants for fan speed
     #Create a private int data (fan speed)
     #Create a private bool data (fan power(on/off))

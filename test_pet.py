262665e8a0c89818df4c52b8610ca7eeabca52d1
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 14:03:48 +0800

    Program 3: Working Program (Satisfies the given prog instructions)

--- test_pet.py
+++ test_pet.py
@@ -1,8 +1,6 @@
 #This python file will serve as the main file that will be run.
 
-#write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. 
-#This data should be stored as the object’s attributes. 
-#Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+#A python program that lets the user input the pet's name, type ,and age. Then, displaying them through certain methods.
 
 from pet_class import Pet
 

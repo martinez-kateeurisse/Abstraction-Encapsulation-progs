e4e557fb11cb562cf64ead692ae731a1591a96fb
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 14:41:14 +0800

    Program 3: Minor changes(comments added)

--- pet_ui.py
+++ pet_ui.py
@@ -5,22 +5,29 @@ from pet_class import Pet
 
 #Create a class
 class UserInterface():
-    #Initialize object values
+    #Initialize object values to store the data 
     def __init__(self):
         self.pet = Pet(" "," "," ")
-    #Asking for user inputs
+    
+    #Asking for user inputs and setting values
     def pet_name (self):
+        # Ask user to enter the name
         self.name = input("Please enter the pet's name: ")
         #Set the name based on user's input
         self.pet.set_name(self.name)
+
     def pet_type (self):
+        # Ask user to enter the type
         self.animal_type = input("Please enter the pet's animal type: ")
         #Set the animal type based on user's input
         self.pet.set_animal_type(self.animal_type)
+
     def pet_age (self):
+        # Ask user to enter the age
         self.age = float(input("Please enter pet's age: ")) 
         #Set the nage base on user's input
         self.pet.set_age(self.age)
+
     #Displaying pet's details
     def pet_details(self):
         print("PET DETAILS")

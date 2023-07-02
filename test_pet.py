9afd68a9581ae8fbdfa183fc1ada3ae664dfa415
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 12:04:21 +0800

    Program 3: test/main file - created(w/prog instructions)

---
+++ test_pet.py
@@ -0,0 +1,5 @@
+#This python file will serve as the main file that will be run.
+
+#write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. 
+#This data should be stored as the object’s attributes. 
+#Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.

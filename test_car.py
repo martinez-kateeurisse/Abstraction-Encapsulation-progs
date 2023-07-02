65a454b1693bfc137f20465f4a36ec8b6a72a0dd
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 09:49:26 +0800

    Program 2: car testing program-Pseudocode

--- test_car.py
+++ test_car.py
@@ -2,3 +2,13 @@
 #After each call to the accelerate method ,get the current speed of the car and display it. 
 #Then call the brake method five times. 
 #After each call to the brake method, get the current speed of the car and display it.
+
+#Import class
+
+#Create car object
+#Call the accelerate method (5 times)
+    #Get speed (each time)
+    #Display speed (each time)
+#Call the brake method (5 times)
+    #Get speed (each time)
+    #Display speed (each time)

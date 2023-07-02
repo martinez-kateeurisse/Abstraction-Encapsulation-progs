5680f5d7811e697dcc6836fbd931fedd3ade74be
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 09:45:44 +0800

    Program 2: car class testing program-created

---
+++ test_car.py
@@ -0,0 +1,4 @@
+#Next, design a program that creates a Car object then calls the accelerate method five times.
+#After each call to the accelerate method ,get the current speed of the car and display it. 
+#Then call the brake method five times. 
+#After each call to the brake method, get the current speed of the car and display it.

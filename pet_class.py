42228fab264a1dda8883e9ea14a11fa0a7aee4e1
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 11:25:33 +0800

    Program 3: class pet - created

--- pet_class.py
+++ pet_class.py
@@ -22,8 +22,9 @@
 
 #Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. This data should be stored as the object’s attributes. Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
 
-#Pseudocode
-#Create a class 
+
+#Create a class
+class Pet():
 #Add a constructor
 #Create data attributes
     #Pet name

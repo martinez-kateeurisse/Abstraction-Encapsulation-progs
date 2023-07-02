f5856a55ba7834209a27a39adf99c2ce7cd1bd34
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 12:12:15 +0800

    Program 3: Pseudocode - added (code flow)

--- test_pet.py
+++ test_pet.py
@@ -3,3 +3,11 @@
 #write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. 
 #This data should be stored as the object’s attributes. 
 #Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+
+ 
+#Create an object of the classs
+    # Ask user to enter the name
+    # Ask user to enter the type
+    # Ask user to enter the age
+#Store the data as object attributes
+#Use methods to retrieve and display output

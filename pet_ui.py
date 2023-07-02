36ccdb36f3e1ad17fa62a604a2e1684fd2ab16bc
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-07-02 14:38:36 +0800

    Program 3: UI file created (methods-> transfered)

---
+++ pet_ui.py
@@ -0,0 +1,29 @@
+#This python file will include the methods that involves user-interface.
+
+#Import fan class
+from pet_class import Pet
+
+#Create a class
+class UserInterface():
+    #Initialize object values
+    def __init__(self):
+        self.pet = Pet(" "," "," ")
+    #Asking for user inputs
+    def pet_name (self):
+        self.name = input("Please enter the pet's name: ")
+        #Set the name based on user's input
+        self.pet.set_name(self.name)
+    def pet_type (self):
+        self.animal_type = input("Please enter the pet's animal type: ")
+        #Set the animal type based on user's input
+        self.pet.set_animal_type(self.animal_type)
+    def pet_age (self):
+        self.age = float(input("Please enter pet's age: ")) 
+        #Set the nage base on user's input
+        self.pet.set_age(self.age)
+    #Displaying pet's details
+    def pet_details(self):
+        print("PET DETAILS")
+        print("Pet's Name: ", self.pet.get_name())
+        print("Pet's Animal Type: ", self.pet.get_animal_type())
+        print("Pet's Age: ", self.pet.get_age())      
